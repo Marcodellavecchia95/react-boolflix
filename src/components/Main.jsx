511edd995ac83c4e281ab0d7a0da63d4b9f10a08
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-06-05 15:49:09 +0200

    fix flags functions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,29 +2,40 @@ import { useMovie } from "./contexts/MovieContext";
 
 export default function Main() {
   const { filteredMovies } = useMovie();
+  // en => us, gb
 
+  const getCorrectFlagName = (lang) => {
+    const flagDictionary = {
+      en: "gb",
+      ja: "jp",
+      ko: "kr",
+      da: "dk",
+    };
+
+    const result = flagDictionary[lang] ?? lang;
+
+    return result;
+  };
+
+  const movieLanguage = (lang) => {
+    const flagName = getCorrectFlagName(lang);
+
+    return `https://flagsapi.com/${flagName.toUpperCase()}/flat/64.png`;
+  };
   return (
     <section>
       <ul>
         {filteredMovies.map((movie) => {
-          const flagLanguage = () => {
-            if (movie.original_language === "en") {
-              movie.original_language = "gb";
-            }
-            return movieLanguage();
-          };
-
-          const movieLanguage = () => {
-            return `https://flagsapi.com/${movie.original_language.toUpperCase()}/flat/64.png`;
-          };
-
           return (
             <li key={movie.id}>
               <h3>{movie.title}</h3>
               <p>Titolo Originale: {movie.original_title}</p>
               <p>
                 Lingua:
-                <img src={flagLanguage()} alt="" />
+                <img
+                  src={movieLanguage(movie.original_language)}
+                  alt={movie.original_language}
+                />
               </p>
               <p>Voto: {movie.vote_average}</p>
             </li>
